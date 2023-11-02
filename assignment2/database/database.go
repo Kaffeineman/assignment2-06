463c7f431f7f252e85bbe6d2dd097ec4fe23f6fc
Kaffeineman <143915409+Kaffeineman@users.noreply.github.com>
2023-11-03 01:31:48 +0700

    Add files via upload

---
+++ assignment2/database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/adindazenn/assignment2-03/assignment2/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+)
+
+func InitDB() (*gorm.DB, error) {
+	config := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(config), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	db.Debug().AutoMigrate(model.Item{}, model.Order{})
+
+	return db, nil
+}
